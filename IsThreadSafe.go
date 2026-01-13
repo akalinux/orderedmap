32f3415aafcf428d2902ed3dc5a18a25b2981efd
mshipper <mshipper@yahoo.com>
2026-01-13 05:01:59 +0000

    Work in progres!

--- IsThreadSafe.go
+++
@@ -1,6 +0,0 @@
-package omap
-
-// This interface is a soap box.. I would like more packages in go to give programatic way of denoting
-// if a instance is thread safe.  If people do, that is great!
-type IsThreadSafe interface {
-}
