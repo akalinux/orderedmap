50017dd6904f8db63770bbf9da4f8205022e4d57
mshipper <mshipper@yahoo.com>
2026-03-02 01:12:04 +0000

    Lessindex speedup.. but not enough to close the gap to Compare

--- LessTree.go
+++ LessTree.go
@@ -4,57 +4,57 @@ func LessIndex[K any, V any](k K, Less func(a, b K) bool, Slice []KvSet[K, V]) (
 
 	end := len(Slice)
 
-	Cmp := func(id int) int {
-		if Less(k, Slice[id].Key) {
-			return -1
-		} else if Less(Slice[id].Key, k) {
-			return 1
-		} else {
-			return 0
-		}
-	}
-
 	if end < 3 {
 		switch end {
 		case 0:
 			return 0, 0
 		case 1:
-			return 0, Cmp(0)
+			if Less(k, Slice[0].Key) {
+				return 0, -1
+			} else if Less(Slice[0].Key, k) {
+				return 0, 1
+			}
+			return 0, 0
 		case 2:
 			// being lazy for just 2 elements
-			offset = Cmp(0)
-			if offset < 1 {
-				return 0, offset
+			for id := range 2 {
+				if Less(k, Slice[id].Key) {
+					return 0, -1
+				} else if !Less(Slice[id].Key, k) {
+					return 0, 0
+				}
 			}
-			return 1, Cmp(1)
+			return 1, 1
 		}
 		return
 	}
 
 	mid := getMid(end)
-	offset = Cmp(mid)
 	idx = mid
 	end--
 	begin := 0
 	diff := 0
 	for {
-		switch offset {
-		case 0:
-			return mid, 0
-		case -1:
-			// moving left
+		lt := Less(k, Slice[mid].Key)
+		if lt {
 			end = mid - 1
 			diff = end - begin
-		case 1:
+		} else if Less(Slice[mid].Key, k) {
 			begin = mid + 1
 			diff = end - begin
+		} else {
+			return mid, 0
 		}
 		if diff == 0 {
-			return begin, Cmp(begin)
+			if Less(k, Slice[begin].Key) {
+				return begin, -1
+			} else if Less(Slice[begin].Key, k) {
+				return begin, 1
+			}
+			return begin, 0
 		} else if diff < 0 {
 			return begin, -1
 		}
 		mid = begin + getMid(diff+1)
-		offset = Cmp(mid)
 	}
 }
